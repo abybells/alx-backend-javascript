92c1468428111e037d466f431776d4f7a59b5cc3
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2023-02-21 18:38:04 +0100

    display message
    basic JS with NodeJS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ */
+function displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
